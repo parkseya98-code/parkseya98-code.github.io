3d22f8cbd709765632d4029c4fac7c1918244a59
srnqmtul <parkseya98@gmail.com>
2026-01-17 11:03:00 +0800

    Update guest.js

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,10 +255,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Lukman and Wahyuni',
+            dates: `${formatDate('2026-01-31 08:00')}/${formatDate('2026-02-1 10:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Dusun Passimbunga Desa Anrihua, Kec. Kindang.',
             ctz: config.get('tz'),
         });
 
